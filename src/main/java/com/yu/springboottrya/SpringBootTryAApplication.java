950dc60e7f7ac4baefff64f24e8b827ae8dad612
yufeng <537777987@qq.com>
2021-07-17 03:50:55 +0800

    #comment SpringBoot项目建立

---
+++ src/main/java/com/yu/springboottrya/SpringBootTryAApplication.java
@@ -0,0 +1,13 @@
+package com.yu.springboottrya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTryAApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootTryAApplication.class, args);
+    }
+
+}
